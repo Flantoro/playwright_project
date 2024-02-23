59df7b4d5a938d1a0e1dbef2d8dcbf09b0d9c9bd
Flantoro <steculalviv@gmail.com>
2024-02-23 12:05:17 +0200

    Error fixed

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://demoqa.com/');
+  await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle('DEMOQA');
+  await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('Selenium certification training', async ({ page }) => {
+test('get started', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
